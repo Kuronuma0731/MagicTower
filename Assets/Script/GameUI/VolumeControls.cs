d22f7c6133558e31751ab1bab4e0ee45dc1d14d5
uronuma K <you11337799@gmail.com>
2025-02-18 22:07:25 +0800

    更正button 按下去腳色暫停不能移動，並關閉button時開啟移動。

--- Assets/Script/GameUI/VolumeControls.cs
+++ Assets/Script/GameUI/VolumeControls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,9 @@ public class VolumeControls : MonoBehaviour
     [SerializeField] private Text volumeText;
     [SerializeField] private Image volumeImage;
     [SerializeField] private Characters Characters;
+    [SerializeField] private Button buttonEvent;
+
+    private bool canMove = true;
     // Update is called once per frame
 
     private void Start()
@@ -19,6 +23,7 @@ public class VolumeControls : MonoBehaviour
         volumeText.text = $"音量: 100 % ";
         //
         volumSlider.onValueChanged.AddListener(SetVloume);
+        buttonEvent.onClick.AddListener(ToggleCharacterMovement);
     }
     public void SetVloume(float volume)
     {
@@ -34,20 +39,28 @@ public class VolumeControls : MonoBehaviour
             volumeText.text = "音量: " + Mathf.RoundToInt(volume * 100) + "%";
         }
     }
+    //按鈕動作時自動隱藏音量
     public void ToggleVolumeControl()
     {
         volumeImage.gameObject.SetActive(volumeImage.gameObject.activeSelf);
         volumSlider.gameObject.SetActive(volumSlider.gameObject.activeSelf); // 顯示或隱藏音量 Slider
-        
-        //Characters.SetCanMove(true);
+
         if (volumeText != null)
         {
             volumeImage.gameObject.SetActive(!volumeImage.gameObject.activeSelf);
             volumeText.gameObject.SetActive(!volumeText.gameObject.activeSelf); // 顯示或隱藏音量顯示文本
             volumeText.gameObject.SetActive(!volumeText.gameObject.activeSelf);
-            //  Characters.SetCanMove(false);
+
             //return;
         }
 
+    }
+
+    // 偵測按鈕動作
+    void ToggleCharacterMovement()
+    {
+
+        canMove = !canMove;
+        Characters.SetCanMove(canMove);
     }
 }
